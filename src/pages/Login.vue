8d8b312710a4e3d378f4b36bd6a259c84bab257f
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-05-26 17:24:04 -0300

    automplete off in login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,7 +9,7 @@
   <form v-on:submit="handleSubmit" class="flex flex-col justify-center items-start gap-3 border-2 p-4 w-[90%] max-w-[600px] mx-auto rounded">
     <h1 class="sm:text-3xl text-2xl w-full bg-blue-50 text-center py-2 text-blue-900/60">Entre na sua conta</h1>
       <label for="user">Usuário</label>
-      <input v-model="user_value" required class="w-full bg-slate-200 p-2 rounded" type="text" name="user" id="user">
+      <input autocomplete="off" v-model="user_value" required class="w-full bg-slate-200 p-2 rounded" type="text" name="user" id="user">
       <label for="password">Senha</label>
       <input v-model="password_value" required class="w-full bg-slate-200 p-2 rounded" type="password" name="password" id="password">
       <input class="bg-blue-500 text-white rounded-[7px] px-4 py-2 cursor-pointer" type="submit" value="Entrar">
