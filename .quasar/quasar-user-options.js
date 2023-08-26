0d98b83ea9011ce2729cf982636c5f393a3322f3
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-08-26 16:05:04 -0300

    fix to deply 2

--- .quasar/quasar-user-options.js
+++ .quasar/quasar-user-options.js
@@ -15,7 +15,9 @@ import lang from 'quasar/lang/pt-BR'
 
 
 
+import {Notify} from 'quasar'
 
 
-export default { config: {"brand":{"primary":"#22487b","secondary":"#a6c546","accent":"#9C27B0","dark":"#1d1d1d","dark-page":"#121212","positive":"#0d8529","negative":"#C10015","info":"#31CCEC","warning":"#F2C037"}},lang }
+
+export default { config: {"brand":{"primary":"#22487b","secondary":"#a6c546","accent":"#9C27B0","dark":"#1d1d1d","dark-page":"#121212","positive":"#0d8529","negative":"#C10015","info":"#31CCEC","warning":"#F2C037"}},lang,plugins: {Notify} }
 
--- src/boot/VerifyToken.js
+++ src/boot/VerifyToken.js
@@ -1,17 +1,17 @@
 import { api } from "./axios";
 import { useTokenStore } from "src/stores/token";
 
-const store = await useTokenStore()
+const store = useTokenStore()
 const {setToken} = store
 
 export default async function verifyToken(config) {
   try {
-    const token = await sessionStorage.getItem('access_token')
+    const token = sessionStorage.getItem('access_token')
     const oldToken = {"oldToken" : token.value};
     const response = await api.post('/refresh/token',  oldToken );
-    const newAccessToken = await response.data.access_token;
-    await sessionStorage.setItem('access_token', newAccessToken)
-    await setToken(newAccessToken)
+    const newAccessToken = response.data.access_token;
+    sessionStorage.setItem('access_token', newAccessToken)
+    setToken(newAccessToken)
 
     return api({
       ...config,
