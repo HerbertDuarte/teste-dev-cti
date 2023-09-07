2f52b9b1d23637ec7972ceb111fbff703b309f9d
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-09-07 13:33:16 -0300

    add envs in quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -52,7 +52,12 @@ module.exports = configure(function (/* ctx */) {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node16'
       },
-      env: require('dotenv').config().parsed,
+      env: {
+        // You have to manually define all the variables you want to pass in
+        API_URL: process.env.API_URL,
+        LOGIN_URL : process.env.LOGIN_URL,
+        // ...
+      },
       vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
